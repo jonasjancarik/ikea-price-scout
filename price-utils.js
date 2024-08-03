dbd5ae7dfa906db5e16429c71bcb89f74c7f2c94
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-08-03 23:59:10 +0200

    chore: revert change from previous commit, fixes some conversions

--- price-utils.js
+++ price-utils.js
@@ -39,7 +39,7 @@ var IkeaPriceUtils = (function () {
         if (!result.isAvailable) {
             return { convertedPrice: null, percentageDiff: null };
         }
-        const comparisonPriceNum = parseFloat(result.price.replace(' ', '').replace('.', '').replace(',', '.'));
+        const comparisonPriceNum = parseFloat(result.price.replace(' ', '').replace(',', '.'));  // we can't just .replace('.', '') because it will mess up the price in repeat calculations (?) needs investigation
         let exchangeRate = IkeaExchangeRates.getRates()[result.currencyCode] || 1;
         const convertedPrice = comparisonPriceNum * exchangeRate;
         const percentageDiff = ((convertedPrice - localPriceNum) / localPriceNum * 100).toFixed(1);
