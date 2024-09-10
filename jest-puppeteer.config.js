d4e4aaab7c5ac6bd5f3adf9c8a0638914850f148
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-09-10 12:24:38 +0200

    Update GitHub Actions and Puppeteer config for dev build and sandboxing.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 module.exports = {
     launch: {
-        headless: false,
+        headless: process.env.CI === 'true' ? 'new' : false,
         args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
             `--disable-extensions-except=${path.join(__dirname, 'dist')}`,
             `--load-extension=${path.join(__dirname, 'dist')}`,
         ],
