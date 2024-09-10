3d0861ecc7cf10064614d6fea23ea0a1ca349f19
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-09-10 12:31:38 +0200

    use just false/true for headless mode

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
     launch: {
-        headless: process.env.CI === 'true' ? 'new' : false,
+        headless: process.env.CI === 'true',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             `--disable-extensions-except=${path.join(__dirname, 'dist')}`,
             `--load-extension=${path.join(__dirname, 'dist')}`,
         ],
-    },
+    },  
 };
